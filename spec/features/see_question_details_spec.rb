0984df9a830602843ae71fdbe38160ac0a61c48a
Lincoln Kupke <lincolnkupke@gmail.com>
2016-05-12 12:30:07 -0400

    Fixed view_all_question spec

--- spec/features/see_question_details_spec.rb
+++ spec/features/see_question_details_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 feature "see question" do
   scenario "see details about posted question" do
-    user1 = FactoryGirl.create(:user)
+    user1 = create(:user)
 
     question2 = Question.create!(title:"Why is rails becoming a lot easier? Launch teaching or just getting it", description: "I was wondering why rails was becoming a lot easier? Is it because the articles make sense now?", creator: user1)
     visit questions_path
-  
+
     expect(page).to have_content(question2.title)
     click_link "#{question2.title}"
     expect(page).to have_content(question2.title)
--- spec/features/view_all_questions_spec.rb
+++ spec/features/view_all_questions_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 feature "see questions" do
   scenario "see recently posted questions" do
-    user1 = User.create(fname: "Lincoln", lname:"Kupke")
-    user2 = User.create(fname: "Kenneth", lname:"Kupke")
+    user1 = create(:user)
+    user2 = create(:user, fname: "Jack", lname:"Smith", email: "example@gmail.com")
     question = Question.create(title:"How to I create a Gemfile? New to rails!!", description: "HI I am new to rails and I was wondering how I create a Gemfile?? Do I create it myself or is it auto-generated?", creator: user2)
     question2 = Question.create(title:"Why is rails becoming a lot easier? Launch teaching or just getting it", description: "I was wondering why rails was becoming a lot easier? Is it because the articles make sense now?", creator: user1)
 
